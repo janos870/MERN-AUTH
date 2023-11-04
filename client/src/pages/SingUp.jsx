b6c7668ccfb7b8394b01b41a28db7173d7e08a1d
janos870 <janos.balogh870@gmail.com>
2023-11-04 20:57:15 +0100

    complite sign up page ui

--- client/src/pages/SingUp.jsx
+++ client/src/pages/SingUp.jsx
@@ -1,7 +1,42 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
 
 export default function SingUp() {
   return (
-    <div>SingUp</div>
-  )
+    <main className="p-3 max-w-lg mx-auto">
+      <h1 className="text-3xl text-center font-semibold my-7">Sign Up</h1>
+      <form className="flex flex-col gap-4">
+        <input
+          type="text"
+          name="username"
+          id="username"
+          placeholder="Username"
+          className="bg-slate-100 p-3 rounded-lg"
+        />
+        <input
+          type="text"
+          name="email"
+          id="email"
+          placeholder="Email"
+          className="bg-slate-100 p-3 rounded-lg"
+        />
+        <input
+          type="text"
+          name="password"
+          id="password"
+          placeholder="Password"
+          className="bg-slate-100 p-3 rounded-lg"
+        />
+        <button className="bg-slate-700 text-white rounded-lg uppercase p-3 hover:opacity-95">
+          Sign Up
+        </button>
+      </form>
+      <div className="flex gap-2 mt-5">
+        <p>Have an acount</p>
+        <Link to={"/sign-in"}>
+          <span className="text-blue-500">Sign In</span>
+        </Link>
+      </div>
+    </main>
+  );
 }
