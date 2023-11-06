c3f223708cbdf29c754e7badc690e03f90d99a8c
janos870 <janos.balogh870@gmail.com>
2023-11-06 22:46:40 +0100

    create sign in api route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,7 +1,9 @@
 import User from "../models/user.model.js";
 import bcrypt from "bcryptjs";
-// import { errorHandler } from "../utils/error.js";
+import { errorHandler } from "../utils/error.js";
+import jwt from 'jsonwebtoken';
 
+// signup
 export const signup = async (req, res, next) => {
   const { username, email, password } = req.body;
   const hashedPassword = await bcrypt.hashSync(password, 10);
@@ -10,6 +12,28 @@ export const signup = async (req, res, next) => {
     await newUser.save();
     res.status(200).json({ success: true, message: "Registration successfully." });
   } catch (error) {
-    res.status(500).json({ success: false, message: "Registration failed!" });
+    const customError = errorHandler(500, "Registration failed!");
+    next(customError); 
   }
 };
+
+// signin
+export const signin = async (req, res, next) => {
+  const { email, password } = req.body;
+  try {
+    const validUser = await User.findOne({ email });
+    if (!validUser) return next(errorHandler(404, 'User not found'));
+    const validPassword = bcrypt.compareSync(password, validUser.password);
+    if (!validPassword) return next(errorHandler(401, 'Wrong credentials'));
+    const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
+    const { password: hashedPassword, ...rest } = validUser._doc;
+    const expiryDate = new Date(Date.now() + 3600000); // 1 hour
+    res
+      .cookie('access_token', token, { httpOnly: true, expires: expiryDate })
+      .status(200)
+      .json(rest);
+  } catch (error) {
+    next(error);
+  }
+};
+
--- api/routes/auth.route.js
+++ api/routes/auth.route.js
@@ -1,9 +1,11 @@
 import express from "express";
 import router from "./user.route.js";
 import { signup } from "../controllers/auth.controller.js";
+import { signin } from "../controllers/auth.controller.js";
 
 const app = express.Router();
 
 router.post("/signup", signup);
+router.post("/signin", signin);
 
 export default router;
