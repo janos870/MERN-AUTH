f6834fa525180dfe4544ce24c7b18eb9570500b9
janos870 <janos.balogh870@gmail.com>
2023-11-11 00:30:01 +0100

    complete update user  functionaliti

--- client/src/redux/user/userSlice.js
+++ client/src/redux/user/userSlice.js
@@ -16,16 +16,34 @@ export const userSlice = createSlice({
     signInSuccess: (state, action) => {
       state.currentUser = action.payload;
       state.loading = false;
-      state.error = false; 
+      state.error = false;
     },
     signInFailure: (state, action) => {
       state.loading = false;
       state.error = action.payload;
     },
+    updateUserStart: (state) => {
+      state.loading = true;
+    },
+    updateUserSuccess: (state, action) => {
+      state.currentUser = action.payload,
+      state.loading = false,
+      state.action = false;
+    },
+    updateUserFailure: (state, action) => {
+      state.loading = false,
+      state.error = action.payload
+    }
   },
 });
 
-export const { signInStart, signInSuccess, signInFailure } = userSlice.actions;
+export const {
+  signInStart,
+  signInSuccess,
+  signInFailure,
+  updateUserStart,
+  updateUserSuccess,
+  updateUserFailure
+} = userSlice.actions;
 
 export default userSlice.reducer;
-
